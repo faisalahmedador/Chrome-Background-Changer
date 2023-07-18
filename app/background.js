73c438c6981155f6b1936efcff5db4bbd6b3e3b0
Faisal Ahmed <faisal@jotno.net>
2023-07-18 17:40:23 +0600

    webpack 3

--- app/background.js
+++ app/background.js
@@ -1,19 +1,15 @@
-import {storeColor} from "./storeChormeColor";
 import {getColor} from "./getChromeColor";
 import {changeBackground} from "./changeChormeBackground";
 
-chrome.runtime.onConnect.addListener(function () {
-    console.log('connected')
-})
 
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
     console.log(tabId, 'onUpdated')
     if (changeInfo.status === 'complete' && tab.active) {
         getColor().then(value => {
-            console.log(value)
-            changeBackground(tabId, value.currentColor)
+            if(value.currentColor) {
+                changeBackground(tabId, value.currentColor)
+            }
         })
-        // do your things
 
     }
 })
